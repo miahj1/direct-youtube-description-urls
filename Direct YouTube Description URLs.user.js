1f310fd31e417e05a6f86e9ab95e1da5c23e2c81
Jabed Miah <84815985+miahj1@users.noreply.github.com>
2023-09-05 11:57:53 -0400

    Add files via upload

---
+++ Direct YouTube Description URLs.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Direct YouTube Description URLs
+// @namespace    https://www.jabedmiah.com
+// @version      3.1
+// @description  Changes the masked urls in youtube descriptions to their direct versions.
+// @author       Jabed Miah
+// @match        https://www.youtube.com/watch*
+// @icon         https://www.google.com/s2/favicons?domain=youtube.com
+// @supportURL   https://github.com/miahj1/direct-youtube-description-urls/issues
+// @license      MIT
+// ==/UserScript==
+
+(new MutationObserver(check)).observe(document, {childList: true, subtree: true});
+
+function urlEventHandler() {
+    const description = document.getElementsByClassName("style-scope ytd-text-inline-expander")[0];
+    const allDescUrls = description.getElementsByTagName("a");
+
+    for (let i = 0; i < allDescUrls.length; i++) {
+        if (allDescUrls[i].text.startsWith("https://") === true){
+            allDescUrls[i].href = allDescUrls[i].text;
+        } else if (allDescUrls[i].text.startsWith("http://") == true){
+            let httpToHttps = allDescUrls[i].text.replace('http', 'https')
+            allDescUrls[i].href = httpToHttps;
+            allDescUrls[i].text = httpToHttps;
+        }
+    }
+};
+
+function check(changes, observer) {
+    const metaDataCollapsed = document.getElementsByClassName("item style-scope ytd-watch-metadata")[2]
+    if (metaDataCollapsed) {
+        metaDataCollapsed.addEventListener("click", urlEventHandler)
+        console.log("The element has been clicked.")
+        observer.disconnect();
+    } 
+};
