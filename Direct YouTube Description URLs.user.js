449c3377c79e43bfc87b92ba8e20a42945f25a5b
Jabed Miah <84815985+miahj1@users.noreply.github.com>
2023-09-09 08:58:27 -0400

    Update Direct YouTube Description URLs.user.js
    1. Added condition to check if urls in the description are masked before performing any operations on them. 
    2. Fixed conflict with “YouTube Expand Description . . . “ script by adding a mouseover event.
    3. Refactored code for conciseness and uniformity.

--- Direct YouTube Description URLs.user.js
+++ Direct YouTube Description URLs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Direct YouTube Description URLs
 // @namespace    https://www.jabedmiah.com
-// @version      3.1
+// @version      3.3
 // @description  Changes the masked urls in youtube descriptions to their direct versions.
 // @author       Jabed Miah
 // @match        https://www.youtube.com/watch*
@@ -10,27 +10,32 @@
 // @license      MIT
 // ==/UserScript==
 
-(new MutationObserver(check)).observe(document, {childList: true, subtree: true});
+(new MutationObserver(elementCheck)).observe(document, {childList: true, subtree: true});
 
-function urlEventHandler() {
-    const description = document.getElementsByClassName("style-scope ytd-text-inline-expander")[0];
-    const allDescUrls = description.getElementsByTagName("a");
+function descEventHandler() {
+    const descExpanded = document.getElementsByClassName("style-scope ytd-text-inline-expander")[0];
+    const allDescUrls = descExpanded.getElementsByTagName("a");
 
     for (let i = 0; i < allDescUrls.length; i++) {
-        if (allDescUrls[i].text.startsWith("https://")) {
-            allDescUrls[i].href = allDescUrls[i].text;
-        } else if (allDescUrls[i].text.startsWith("http://")) {
-            let httpToHttps = allDescUrls[i].text.replace('http', 'https')
-            allDescUrls[i].href = httpToHttps;
-            allDescUrls[i].text = httpToHttps;
+        if (allDescUrls[i].href.startsWith("https://www.youtube.com/redirect?event=video_description")){
+            if (allDescUrls[i].text.startsWith("https://")) {
+                allDescUrls[i].href = allDescUrls[i].text;
+            } else if (allDescUrls[i].text.startsWith("http://")) {
+                let httpToHttps = allDescUrls[i].text.replace('http', 'https');
+                allDescUrls[i].href = httpToHttps;
+                allDescUrls[i].text = httpToHttps;
+            }
         }
     }
 };
 
-function check(changes, observer) {
-    const metaDataCollapsed = document.getElementsByClassName("item style-scope ytd-watch-metadata")[2]
-    if (metaDataCollapsed) {
-        metaDataCollapsed.addEventListener("click", urlEventHandler)
+function elementCheck(changes, observer) {
+    const desc = document.getElementsByClassName("item style-scope ytd-watch-metadata")[2];
+
+    if (desc) {
+        ["click", "mouseover"].forEach((event) => {
+            desc.addEventListener(event, descEventHandler);
+        });
         observer.disconnect();
-    } 
+    }
 };
