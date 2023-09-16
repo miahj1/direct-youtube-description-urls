0d2ac37c4e48621302f7ec910f5aaf92c8166b00
Jabed Miah <84815985+miahj1@users.noreply.github.com>
2023-09-15 23:13:24 -0400

    Update Direct YouTube Description URLs.user.js

--- Direct YouTube Description URLs.user.js
+++ Direct YouTube Description URLs.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Direct YouTube Description URLs
 // @namespace    https://www.jabedmiah.com
-// @version      3.3.1
+// @version      3.4
 // @description  Changes the masked urls in youtube descriptions to their direct versions.
 // @author       Jabed Miah
-// @match        https://www.youtube.com/watch*
+// @match        *.youtube.com/watch*
 // @icon         https://www.google.com/s2/favicons?domain=youtube.com
 // @supportURL   https://github.com/miahj1/direct-youtube-description-urls/issues
 // @license      MIT
@@ -14,26 +14,52 @@
 
 function descEventHandler() {
     const descExpanded = document.getElementsByClassName("style-scope ytd-text-inline-expander")[0];
-    const allDescUrls = descExpanded.getElementsByTagName("a");
+    const allDescUrls = descExpanded.querySelectorAll("a[target='_blank']");
 
     for (let i = 0; i < allDescUrls.length; i++) {
-        if (allDescUrls[i].text.startsWith("https://")) {
-            allDescUrls[i].href = allDescUrls[i].text;
-        } else if (allDescUrls[i].text.startsWith("http://")) {
-            let httpToHttps = allDescUrls[i].text.replace('http', 'https');
-            allDescUrls[i].href = httpToHttps;
-            allDescUrls[i].text = httpToHttps;
+        if (allDescUrls[i].href === allDescUrls[i].text) { break; }
+
+        if (isHttps(allDescUrls[i])) {
+            if (isTruncated(allDescUrls[i])) {
+                allDescUrls[i].href = allDescUrls[i].text = ytUriDecoder(allDescUrls[i].href);
+            } else {
+                allDescUrls[i].href = allDescUrls[i].text;
+            }
+        } else if (isHttp(allDescUrls[i])) {
+            if (isTruncated(allDescUrls[i])) {
+                allDescUrls[i].href = allDescUrls[i].text = ytUriDecoder(allDescUrls[i].href).replace('http', 'https');
+            } else {
+                allDescUrls[i].href = allDescUrls[i].text = allDescUrls[i].text.replace('http', 'https');
+            }
         }
     }
+}
+
+function ytUriDecoder(uglyUrl) {
+    let directUrl = uglyUrl.split("q=")[1].split("&v")[0];
+    return directUrl.replaceAll('%3A', ':').replaceAll('%2F', '/');
+}
+
+function isHttp(uglyUrl) {
+    return uglyUrl.text.startsWith("http://");
+}
+
+function isHttps(uglyUrl) {
+    return uglyUrl.text.startsWith("https://");
+}
+
+function isTruncated(uglyUrl) {
+    return uglyUrl.text.endsWith("...");
 }
 
 function elementCheck(changes, observer) {
     const desc = document.getElementsByClassName("item style-scope ytd-watch-metadata")[2];
 
     if (desc) {
-        ["click", "mouseover"].forEach((event) => {
+        ["click", "mouseenter"].forEach((event) => {
             desc.addEventListener(event, descEventHandler);
         });
+
         observer.disconnect();
     }
 }
